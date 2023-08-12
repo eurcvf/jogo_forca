1a72d1acb5b683623eae728a44ff278054111ff3
eurcvf <eurcvf@hotmail.com>
2023-08-12 15:18:49 -0300

    Adicionado lista de palavras

--- palavraforca.py
+++ palavraforca.py
@@ -1,3 +1,3 @@
 import random # biblioteca para conseguir valores randômicos
-lista_palavras = ['neymar', 'isabel', 'roberto', 'leticia', 'silvio', 'jessica', 'udo', 'marco', 'bia', 'vitor', 'leonardo']
+lista_palavras = ['neymar', 'isabel', 'roberto', 'leticia', 'silvio', 'jessica', 'udo', 'marco', 'bia', 'vitor', 'leonardo', 'larissa', 'laura']
 palavras = random.choice(lista_palavras)
